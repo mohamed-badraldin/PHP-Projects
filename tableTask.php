3b8b42d4491b48067ca9f21f4d1f5eaad2dc9564
Mohamed-Badraldin <mohamed.badraldin.mohamed@gmail.com>
2021-06-10 00:01:04 +0200

    dynamic table

---
+++ tableTask.php
@@ -0,0 +1,128 @@
+<?php
+// dynamic table
+// dynamic rows
+// dynamic columns
+// check if gender of user == m ==> male
+// check if gender of user == f ==> female
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ]
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ]
+
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'mena',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ]
+    ]
+];
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>    
+<div class="container mt-3 ">
+    <h1 class="text-center mt-5">Dynamic table</h1>
+    <div class="row m-5">
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                <?php 
+                foreach($users[0] as $key => $value){
+                echo '<th>'.strtoupper($key).'</th>';
+                }
+                ?>
+                </tr>
+            </thead>
+        <tbody>
+        <?php 
+        foreach($users as $obj){
+            echo "<tr>";
+            foreach($obj as $value){
+
+                if(is_string($value) OR is_integer($value)){
+                    echo '<td scope="col">'.$value.'</td>';
+                }
+
+                elseif(is_object($value)){
+                    if($value->gender === "m")
+                    echo '<td scope="col">Male</td>';  
+                    else
+                    echo '<td scope="col">Female</td>';
+                }
+
+                elseif(array_keys($value) !== range(0, count($value) - 1)){
+                    echo '<td>';
+                    foreach($value as $ke => $val){
+                    echo ($ke." : ".$val."</br>");
+                    } 
+                    echo "</td>";
+                }
+
+                else {
+                    echo '<td>';
+                    foreach($value as $valu){
+                    echo  $valu. "</br>";
+                    } 
+                    echo "</td>";
+                }
+            }
+            echo "</tr>";
+        }
+        ?>  
+        </tbody>
+        </table>
+    </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
